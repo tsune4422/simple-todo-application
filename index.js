981c91865f1a8f2f332b8019b89931598c1ec7e3
tsune4422 <a5821073@aoyama.jp>
2022-06-10 17:22:07 +0900

    addtodo

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function addTask(title) {
     const task = { isDone: false }
 
     // やることのタイトルを設定(このままだと、すべてのタイトルがHello Worldになってしまう！)
-    task.title = title
+    task.title =  title
 
     // 以降は無視して良い
     tasks.push(task);
